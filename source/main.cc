e955f80ec59c2fd2fc21fc43de0fca6bc2c13ade
Pavel Kovalenko <paul-kv@yandex.ru>
2014-07-26 21:04:59 +0400

    Remove unused code.

--- source/main.cc
+++ source/main.cc
@@ -2,26 +2,10 @@
 #include <iostream>
 #include <gtkmm/main.h>
 
-void usage()
-{
-  std::cout << "usage: ./main -l {1, 2, 3} (1=easy, 2=normal, 3=hard)" << std::endl;
-  exit(1);
-}
-
 int main (int argc, char *argv[])
 {
-  /*
-  if(argc != 3)
-    usage();
-  int diff = 0;
-  if(std::string(argv[1]) == "-l")
-    diff = atoi(argv[2]);		
-  else
-    usage();
-  */
   Gtk::Main kit(argc, argv);
   MineGui mgui (1);
   Gtk::Main::run(mgui);
   return 0;
 }
-
